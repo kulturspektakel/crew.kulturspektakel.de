844c17a288ddcb8bf73490113cd9cf4283c1600e
Daniel Büchele <daniel@buechele.cc>
2023-12-02 15:18:49 +0000

    update

--- scripts/merge-envs.ts
+++ scripts/merge-envs.ts
@@ -4,27 +4,25 @@ import {parse} from 'dotenv';
 
 const ENV_PROD_PATH = path.join(__dirname, '..', '.env.production');
 const ENV_PATH = path.join(__dirname, '..', '.env');
-const ENV_YML = path.join(__dirname, '..', 'env.json');
+const ENV_YML = path.join(__dirname, '..', '.env.json');
 
 (async () => {
-  const prod = await (
-    await fs.readFile(ENV_PROD_PATH)
+  const dev = await readEnv(ENV_PATH);
+  const prod = await readEnv(ENV_PROD_PATH);
+  const config = {...dev, ...prod};
+  delete config.PORT;
+  await fs.writeFile(ENV_YML, JSON.stringify(config));
+  console.log('Merged configs into .env');
+})();
+
+async function readEnv(path: string) {
+  const config = await (
+    await fs.readFile(path)
   )
     .toString()
     .split('\n')
     // filter empty vars
-    .filter((l) => !l.endsWith('=""'));
-
-  // adding new line
-  prod.unshift('');
-  await fs.appendFile(ENV_PATH, prod.join('\n'));
-
-  const conf = await fs.readFile(ENV_PATH);
-  const p = parse(conf);
-  delete p.PORT;
-
-  await fs.writeFile(ENV_YML, JSON.stringify(p));
-
-  // await fs.unlink(ENV_PROD_PATH);
-  console.log('Merged configs into .env');
-})();
+    .filter((l) => !l.endsWith('=""'))
+    .join('\n');
+  return parse(config);
+}
